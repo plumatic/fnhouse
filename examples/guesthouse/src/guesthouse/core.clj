b9231cf4a0353128e6a8dbfe86ac184863f1adc1
davegolland <dave@getprismatic.com>
2014-03-21 23:48:13 -0700

    small refactor, and adding docstrings to functions and namespaces

--- examples/guesthouse/src/guesthouse/core.clj
+++ examples/guesthouse/src/guesthouse/core.clj
@@ -1,42 +1,27 @@
 (ns guesthouse.core
+  "This is an example project that ties together all of the pieces of fnhouse:
+    1. start with a namespace ('guesthouse.guestbook)
+    2. suck up the handlers in the namespace with fnhouse.handlers library
+    3. wrap each of them with schema coercion middleware from fnhouse.middleware
+    4. compile all the handlers into a single router with fnhouse.routes
+    5. wrap this root handler in standard ring middleware
+    6. start a jetty server"
   (:use plumbing.core)
   (:require
    [clojure.string :as str]
    [ring.adapter.jetty :as jetty]
-   [ring.middleware.json :as json]
-   [ring.middleware.params :as params]
    [fnhouse.handlers :as handlers]
    [fnhouse.middleware :as middleware]
    [fnhouse.routes :as routes]
    [guesthouse.guestbook :as guestbook]
+   [guesthouse.ring :as ring]
    [guesthouse.schemas :as schemas]))
 
 (set! *warn-on-reflection* true)
 
-;; TODO: move ring/jetty stuff into its own file
-
-(defn wrap-exception [f]
-  (fn [request]
-    (try (f request)
-         (catch Exception e
-           (.printStackTrace e)
-           {:status 500
-            :body "Exception caught"}))))
-
-(defn keywordize-middleware [handler]
-  (fn [req]
-    (handler
-     (update-in req [:query-params] keywordize-map))))
-
-(defn ring-middleware [handler]
-  (-> handler
-      keywordize-middleware
-      (json/wrap-json-body {:keywords? true})
-      params/wrap-params
-      json/wrap-json-response
-      wrap-exception))
-
 (def entry-coercer
+  "A custom output coercer that is used to coerce a server-side Entry
+   into a ClientEntry whenever it appears in a response"
   (fn [schema]
     (when (= schema schemas/ClientEntry)
       (fn [request x]
@@ -46,25 +31,32 @@
               (assoc :first-name first
                      :last-name last)))))))
 
-(defn custom-coercion-middleware [handler]
+(defn custom-coercion-middleware
+  "Wrap a handler with the schema coercing middleware"
+  [handler]
   (middleware/coercion-middleware
    handler
    (constantly nil)
    entry-coercer))
 
-(defn run-jetty [options handler]
-  (jetty/run-jetty handler options))
-
-
-;; TODO: make defn, options required
-(defnk start-api
-  [resources
-   {options
-    {:port 6054
-     :join? false}}]
+(defn wrapped-root-handler
+  "Take the resources, partially apply them to the handlers in
+   the 'guesthouse.guestbook namespace, wrap each with a custom
+   coercing middleware, and then compile them into a root handler
+   that will route requests to the appropriate underlying handler.
+   Then, wrap the root handler in some standard ring middleware."
+  [resources]
   (->> resources
        ((handlers/nss->handlers-fn {"guestbook" 'guesthouse.guestbook}))
        (map custom-coercion-middleware)
        routes/root-handler
-       ring-middleware
-       (run-jetty options)))
+       ring/ring-middleware))
+
+(defn start-api
+  "Take resources and server options, and spin up a server with jetty"
+  [resources options]
+  (-> resources
+      wrapped-root-handler
+      (jetty/run-jetty options)))
+
+(set! *warn-on-reflection* false)
--- examples/guesthouse/test/guesthouse/core_test.clj
+++ examples/guesthouse/test/guesthouse/core_test.clj
@@ -6,6 +6,32 @@
    [cheshire.core :as cheshire]
    [guesthouse.schemas :as schemas]))
 
+(def *port* 6054)
+
+(def *base-url*
+  (format "http://localhost:%s/guestbook/" *port*))
+
+(defn http-post [url body]
+  (client/post
+   (str *base-url* url)
+   {:content-type :json
+    :as :json
+    :throw-exceptions false
+    :body (cheshire/generate-string body)}))
+
+(defn http-get [url & [qps]]
+  (client/get
+   (str *base-url* url)
+   (assoc-when
+    {:as :json
+     :throw-exceptions false}
+    :query-params qps)))
+
+(defn http-delete [url]
+  (client/delete
+   (str *base-url* url)
+   {:as :json
+    :throw-exceptions false}))
 
 ;; TODO: split out supporting functions
 
@@ -14,85 +40,65 @@
   (let [guestbook (atom {})
         resources {:index (atom 0)
                    :guestbook guestbook}
-        server (start-api {:resources resources})
-        base-url "http://localhost:6054/guestbook/"
-        do-post (fn [url body]
-                  (client/post
-                   (str base-url url)
-                   {:content-type :json
-                    :as :json
-                    :throw-exceptions false
-                    :body (cheshire/generate-string body)}))
-        do-get (fn [url & [qps]]
-                 (client/get
-                  (str base-url url)
-                  (assoc-when
-                   {:as :json}
-                   :query-params qps)))
 
-        do-delete (fn [url]
-                    (client/delete
-                     (str base-url url)
-                     {:as :json
-                      :throw-exceptions false}))]
-    (try ;; with-open
-      (let [john-entry {:name "John Doe"
-                        :age 31
-                        :lang :clj}
-            jane-entry {:name "Jane Doe"
-                        :age 30
-                        :lang :cljs}
-            client-john-entry {:lang "clj"
-                               :age 31
-                               :index 1
-                               :first-name "John"
-                               :last-name "Doe"}
-            client-jane-entry {:lang "cljs"
-                               :age 30
-                               :index 2
-                               :first-name "Jane"
-                               :last-name "Doe"}]
-        (is (= client-john-entry
-               (:body (do-post "entries" john-entry))))
+        john-entry {:name "John Doe"
+                    :age 31
+                    :lang :clj}
+        jane-entry {:name "Jane Doe"
+                    :age 30
+                    :lang :cljs}
+        client-john-entry {:lang "clj"
+                           :age 31
+                           :index 1
+                           :first-name "John"
+                           :last-name "Doe"}
+        client-jane-entry {:lang "cljs"
+                           :age 30
+                           :index 2
+                           :first-name "Jane"
+                           :last-name "Doe"}
+        server (start-api resources {:port *port* :join? false})]
+    (try
+      (is (= client-john-entry
+             (:body (http-post "entries" john-entry))))
 
-        (is (= (assoc john-entry :index 1)
-               (safe-get @guestbook 1)))
+      (is (= (assoc john-entry :index 1)
+             (safe-get @guestbook 1)))
 
-        (is (= client-jane-entry
-               (:body (do-post "entries" jane-entry))))
+      (is (= client-jane-entry
+             (:body (http-post "entries" jane-entry))))
 
-        (is (= #{client-john-entry client-jane-entry}
-               (set (:body (do-get "entries")))))
+      (is (= #{client-john-entry client-jane-entry}
+             (set (:body (http-get "entries")))))
 
-        (is (= [client-john-entry]
-               (:body (do-get "search" {:q "John"}))))
+      (is (= [client-john-entry]
+             (:body (http-get "search" {:q "John"}))))
 
-        (is (= #{client-john-entry client-jane-entry}
-               (set (:body (do-get "search" {:q "Doe"})))))
+      (is (= #{client-john-entry client-jane-entry}
+             (set (:body (http-get "search" {:q "Doe"})))))
 
-        (is (= client-john-entry
-               (:body (do-get "entries/1"))))
+      (is (= client-john-entry
+             (:body (http-get "entries/1"))))
 
-        (testing "update entry"
-          (let [updated-john-entry (update-in john-entry [:age] inc)]
-            (is (= (:body schemas/ack)
-                   (:body (do-post "entries/1" updated-john-entry))))
-
-            (is (= updated-john-entry (dissoc (get @guestbook 1) :index)))
-
-            ;; update it back
-            (is (= (:body schemas/ack) (:body (do-post "entries/1" john-entry)))))
-
-          (is (= 404 (:status (do-post "entries/1234567" john-entry)))))
-
-
-        (testing "delete"
-          (assert (get @guestbook 1))
+      (testing "update entry"
+        (let [updated-john-entry (update-in john-entry [:age] inc)]
           (is (= (:body schemas/ack)
-                 (:body (do-delete "entries/1"))))
+                 (:body (http-post "entries/1" updated-john-entry))))
 
-          (is (nil? (get @guestbook 1)))
+          (is (= updated-john-entry (dissoc (get @guestbook 1) :index)))
 
-          (is (= 404 (:status (do-delete "entries/123456"))))))
+          ;; update it back
+          (is (= (:body schemas/ack) (:body (http-post "entries/1" john-entry))))
+
+          (is (= 404 (:status (http-post "entries/1234567" john-entry))))))
+
+      (testing "delete"
+        (assert (get @guestbook 1))
+        (is (= (:body schemas/ack)
+               (:body (http-delete "entries/1"))))
+
+        (is (nil? (get @guestbook 1)))
+
+        (is (= 404 (:status (http-delete "entries/123456")))))
 
       (finally (.stop server)))))
